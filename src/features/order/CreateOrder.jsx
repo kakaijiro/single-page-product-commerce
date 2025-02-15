2adaeeb21c87da04dd9a748090f4a40553806202
jirokk <kakaijiro@gmail.com>
2025-02-15 10:13:29 +0900

    implement order creation functionality with form handling and validation

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,4 +1,12 @@
 import { useState } from "react";
+import {
+  Form,
+  Navigate,
+  redirect,
+  useActionData,
+  useNavigation
+} from "react-router-dom";
+import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -31,6 +39,10 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
+  const navigation = useNavigation();
+  const isSubmitting = Navigate.state === "submitting";
+
+  const formErrors = useActionData();
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
 
@@ -38,7 +50,8 @@ function CreateOrder() {
     <div>
       <h2>{`Ready to order? Let's go!`}</h2>
 
-      <form>
+      {/* <Form method="POST" action="/order/new"> */}
+      <Form method="POST">
         <div>
           <label>First Name</label>
           <input type="text" name="customer" required />
@@ -49,6 +62,7 @@ function CreateOrder() {
           <div>
             <input type="tel" name="phone" required />
           </div>
+          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
         <div>
@@ -68,13 +82,49 @@ function CreateOrder() {
           />
           <label htmlFor="priority">Want to yo give your order priority?</label>
         </div>
-
         <div>
-          <button>Order now</button>
+          <input type="hidden" name="cart" value={JSON.stringify(cart)} />
+          <button disabled={isSubmitting}>
+            {isSubmitting ? "Placing order now ..." : "Order now"}
+          </button>
         </div>
-      </form>
+      </Form>
     </div>
   );
+}
+
+// React Router can work just with <Form></Form> and action function. No need for javascript code and submit handlers, futhermore, no need for creating state variables here
+
+// Whenever the Form is submitted behind the scenes, the Router calls this function action and passes in the submitted request.
+export async function action({ request }) {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  // parse data
+  try {
+    const parsedData = JSON.parse(data.cart);
+    if (!Array.isArray(parsedData)) throw new Error("Invalid cart data");
+
+    const finalData = {
+      ...data,
+      cart: parsedData,
+      priority: data.priority === "on"
+    };
+
+    const errors = {};
+    if (!isValidPhone(finalData.phone))
+      errors.phone =
+        "Please enter your correct phone number. We might need it to contact you.";
+    if (Object.keys(errors).length > 0) return errors;
+
+    // if appropriate data is presented, create a new order and redirect it
+    const newOrder = await createOrder(finalData);
+    return redirect(`/order/${newOrder.id}`);
+  } catch (error) {
+    console.error("Error parsing cart data", error);
+  }
+
+  return null;
 }
 
 export default CreateOrder;
