ee03071edfa64e965e65e79a8e04825bb604db6e
jirokk <kakaijiro@gmail.com>
2025-02-17 19:31:54 +0900

    refactor CartItem component to improve product display and layout

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -4,11 +4,24 @@ import { formatCurrency } from "../../utils/helpers";
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
 
+  // return (
+  //   <li className="py-3 sm:flex sm:items-center sm:justify-between">
+  //     <p className="mb-1 sm:mb-0">
+  //       {quantity}&times; {name}
+  //     </p>
+  //     <div className="flex items-center justify-between sm:gap-6">
+  //       <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
+  //       <Button type="small">Delete</Button>
+  //     </div>
+  //   </li>
+  // );
+
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-between">
-      <p className="mb-1 sm:mb-0">
-        {quantity}&times; {name}
-      </p>
+      <div className="mb-1 sm:mb-0">
+        <p>product: {name}</p>
+        <p>quantity:{quantity}</p>
+      </div>
       <div className="flex items-center justify-between sm:gap-6">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
         <Button type="small">Delete</Button>
